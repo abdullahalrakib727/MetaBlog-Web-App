8e2eaeb5ffb03d9a908c18bcc5a166d6ab1a2512
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2023-11-12 12:09:00 +0600

    added add blog page

--- src/pages/AddBlog/AddBlog.jsx
+++ src/pages/AddBlog/AddBlog.jsx
@@ -1,22 +1,37 @@
+import { useContext } from "react";
+import { AuthContext } from "../../Providers/AuthProvider";
+
 const AddBlog = () => {
+    const {user} = useContext(AuthContext);
+    console.log(user)
+ const handleSubmit = (e)=>{
+    e.preventDefault();
+    const title = e.target.title.value;
+    const photoUrl = e.target.photo.value;
+    const category = e.target.category.value;
+    const shortDescription = e.target.short.value;
+    const longDescription = e.target.long.value;
+    const published = new Date();
+    const authorEmail = user?.email;
+    const authorName = user?.displayName;
+    console.log({shortDescription,longDescription, published, authorEmail,authorName})
 
-
-    // title,image url,category,short 
-    // description, long description
+ }
   return (
-    <div className="container mx-auto bg-base-200">
-      <div className="min-h-screen">
+    <div className="container mx-auto">
+      <div className="min-h-screen flex justify-center items-center">
         <div className="card w-full shadow-2xl bg-base-100">
-          <form className="card-body">
+          <form className="card-body" onSubmit={handleSubmit}>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Title</span>
               </label>
               <input
                 type="text"
+                name="title"
                 placeholder="Title of the Blog"
                 className="input input-bordered"
-                required
+                // required
               />
             </div>
             <div className="form-control">
@@ -25,47 +40,50 @@ const AddBlog = () => {
               </label>
               <input
                 type="text"
+                name="photo"
                 placeholder="Url of the image"
                 className="input input-bordered"
-                required
+                // required
               />
             </div>
             {/* select will be used */}
             <div className="form-control">
               <label className="label">
-                <span className="label-text">Category</span>
+                <span className="label-text">Choose a Category</span>
               </label>
-              <input
-                type="text"
-                placeholder="Url of the image"
-                className="input input-bordered"
-                required
-              />
+              <select name="category" className="select select-bordered w-full">
+                <option disabled selected>
+                  Select a Category
+                </option>
+                <option>Food</option>
+                <option>Health</option>
+                <option>Entertainment</option>
+                <option>Tech</option>
+              </select>
             </div>
             <div className="form-control">
               <label className="label">
-                <span className="label-text">Image URL</span>
+                <span className="label-text">Short Description</span>
               </label>
-              <input
-                type="text"
-                placeholder="Url of the image"
-                className="input input-bordered"
-                required
-              />
+              <textarea
+              name="short"
+                className="textarea textarea-bordered"
+                placeholder="Short Description"
+              ></textarea>
             </div>
+
             <div className="form-control">
               <label className="label">
-                <span className="label-text">Image URL</span>
+                <span className="label-text">Long Description</span>
               </label>
-              <input
-                type="text"
-                placeholder="Url of the image"
-                className="input input-bordered"
-                required
-              />
+              <textarea
+              name="long"
+                className="textarea textarea-bordered"
+                placeholder="Long Description"
+              ></textarea>
             </div>
             <div className="form-control mt-6">
-              <button className="btn btn-primary">Login</button>
+              <button className="btn register-btn normal-case">Submit</button>
             </div>
           </form>
         </div>
