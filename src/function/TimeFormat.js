b711e11944978b22399fbd646aa1a78995986a0e
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2023-11-13 23:51:34 +0600

    added timeformat function

---
+++ src/function/TimeFormat.js
@@ -0,0 +1,37 @@
+const TimeFormat = (published ) => {
+  const timestamp = published;
+
+  const date = new Date(timestamp);
+
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const year = date.getFullYear();
+  const month = monthNames[date.getMonth()];
+  const day = String(date.getDate()).padStart(2, "0");
+  let hours = date.getHours();
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+
+  const ampm = hours >= 12 ? "PM" : "AM";
+
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+
+  const formattedTime = `${day}-${month}-${year} ${hours}:${minutes} ${ampm}`;
+
+  return formattedTime;
+};
+
+export default TimeFormat;
--- src/pages/Blog Details/BlogDetail.jsx
+++ src/pages/Blog Details/BlogDetail.jsx
@@ -4,6 +4,7 @@ import { Helmet } from "react-helmet";
 import { Link, useLoaderData } from "react-router-dom";
 import { AuthContext } from "../../Providers/AuthProvider";
 import Comment from "./Comments/Comment";
+import TimeFormat from "../../function/TimeFormat";
 
 const BlogDetail = () => {
   const { user } = useContext(AuthContext);
@@ -21,38 +22,9 @@ const BlogDetail = () => {
     authorEmail,
   } = data;
 
-  const timestamp = published;
+  const formattedTime = TimeFormat(published);
 
-  const date = new Date(timestamp);
-
-  const monthNames = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-
-  const year = date.getFullYear();
-  const month = monthNames[date.getMonth()];
-  const day = String(date.getDate()).padStart(2, "0");
-  let hours = date.getHours();
-  const minutes = String(date.getMinutes()).padStart(2, "0");
-
-  const ampm = hours >= 12 ? "PM" : "AM";
-
-  hours = hours % 12;
-  hours = hours ? hours : 12;
-
-  const formattedTime = `${day}-${month}-${year} ${hours}:${minutes} ${ampm}`;
-
+  
   const handleAddComment = (e) => {
     e.preventDefault();
     const form = e.target;
