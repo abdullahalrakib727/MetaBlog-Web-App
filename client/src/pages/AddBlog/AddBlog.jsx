522f08f1ad534c5752a796fc5c94842a3bdbbb9b
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-01-31 04:37:55 +0600

    added new blogs

--- client/src/pages/AddBlog/AddBlog.jsx
+++ client/src/pages/AddBlog/AddBlog.jsx
@@ -7,6 +7,7 @@ import useAxiosSecure from "../../hooks/useAxiosSecure";
 import { useState } from "react";
 import JoditEditor from "jodit-react";
 import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 const AddBlog = () => {
   const { user } = useContext(AuthContext);
@@ -17,6 +18,8 @@ const AddBlog = () => {
   const [content, setContent] = useState();
 
   const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -49,16 +52,16 @@ const AddBlog = () => {
 
       if (res.data.insertedId) {
         toast.success("Blog has been added!");
+        navigate("/all");
         form.reset();
       }
     } catch (error) {
-      toast.error("error",error);
+      toast.error("error", error);
     } finally {
       setIsSubmitting(false);
     }
   };
 
-  
   return (
     <div className="my-10">
       <Helmet>
@@ -66,6 +69,9 @@ const AddBlog = () => {
       </Helmet>
       <div className="min-h-screen flex justify-center items-center">
         <div className="card w-full shadow-md">
+          <h1 className="dark:text-white text-center text-2xl font-semibold">
+            Write a a blog from here
+          </h1>
           <form className="card-body" onSubmit={handleSubmit}>
             <div className="form-control">
               <label className="label">
@@ -118,13 +124,12 @@ const AddBlog = () => {
             </div>
             <div className="form-control mt-6">
               <button className="btn  dark:bg-white text-black uppercase">
-                Submit
+                Add
               </button>
             </div>
           </form>
         </div>
       </div>
-    
     </div>
   );
 };
