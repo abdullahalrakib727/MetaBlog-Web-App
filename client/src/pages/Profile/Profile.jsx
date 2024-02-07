bca4a5ed11c0be0bee3e738377ae5757596abab6
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-02-07 15:59:15 +0600

    added dark mode on profile page

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -30,22 +30,22 @@ const Profile = () => {
             <PdSkeleton />
           </ChakraProvider>
         ) : (
-          <section className="max-w-[1216px] max-h-[344px] bg-[#F6F6F7] p-12 rounded-xl mb-12">
-            <div className="flex flex-col justify-center items-center max-w-[648px] max-h-[284px] mx-auto">
+          <section className="max-w-[1216px] lg:max-h-[344px] bg-[#F6F6F7] dark:bg-[#242535] dark:text-white p-12 rounded-xl mb-12">
+            <div className="flex flex-col justify-center items-center max-w-[648px] lg:max-h-[284px] mx-auto">
               <div className="flex items-center gap-4 mb-6">
                 <img
                   className="max-w-[64px] max-h-[64px] object-contain rounded-full"
                   src={user?.photoURL}
-                  alt=""
+                  alt="user-image"
                 />
                 <div>
-                  <h4 className="font-medium text-xl text-[#181A2A]">
+                  <h4 className="font-medium text-xl text-[#181A2A] dark:text-white">
                     {user?.displayName}
                   </h4>
-                  <p className="text-[#696A75] font-normal text-sm">Role</p>
+                  <p className="text-[#696A75] font-normal text-sm dark:text-[#BABABF]">Role</p>
                 </div>
               </div>
-              <p className="text-center text-[#3B3C4A] text-lg">
+              <p className="text-center text-[#3B3C4A] text-lg dark:text-[#BABABF]">
                 Meet {user?.displayName}, a passionate writer and blogger with a
                 love for technology and travel. {user?.displayName} holds a
                 degree in Computer Science and has spent years working in the
@@ -66,7 +66,7 @@ const Profile = () => {
                 {data.length > 0 ? (
                   <>
                     <h3 className="text-2xl px-4 xl:px-0 font-bold my-5 dark:text-white ">
-                      BLogs by you
+                      Blogs by you
                     </h3>
                     <div className="flex justify-center px-4 xl:px-0">
                       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-4">
