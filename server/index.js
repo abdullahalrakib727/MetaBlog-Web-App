01d35416c01f34de92c079aa887586241606c2b7
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-02-09 06:44:56 +0600

    removed console log

--- server/index.js
+++ server/index.js
@@ -288,9 +288,9 @@ async function run() {
     // await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
