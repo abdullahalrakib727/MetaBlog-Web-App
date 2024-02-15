a99c9d35bcfe2879889f4d11b86c9da6fd89869c
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-02-16 01:19:10 +0600

    added new files

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,26 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-import { RouterProvider } from "react-router-dom";
-
-import "./index.css";
-import router from "./router/Router.js";
-
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-
-import AuthProvider from "./Providers/AuthProvider";
+import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import AuthProvider from './Providers/AuthProvider';
+import { RouterProvider } from 'react-router-dom';
+import router from './router/Router';
 
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <div className="mx-auto">
-      <QueryClientProvider client={queryClient}>
+<QueryClientProvider client={queryClient}>
         <AuthProvider>
           <RouterProvider router={router} />
         </AuthProvider>
       </QueryClientProvider>
-    </div>
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
