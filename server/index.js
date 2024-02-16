090d1c6a93d813a2a6b1f83415923612d2b5e453
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-02-16 19:23:32 +0600

    fixed issues

--- server/index.js
+++ server/index.js
@@ -1,20 +1,22 @@
-import dotenv from "dotenv";
-import express, { Express, Request, Response } from "express";
-import cors from "cors";
-import jwt from "jsonwebtoken";
-import cookieParser from "cookie-parser";
-
-dotenv.config();
-
-const app: Express = express();
-const port: string | Number = process.env.PORT || 5000;
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const app = express();
+const port = process.env.PORT || 5000;
 
 // environment variables
 
 // middleware
 app.use(
   cors({
-    origin: '*',
+    origin: [
+      "https://blog-zone-web.netlify.app",
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://meta-blog-app.vercel.app",
+    ],
     credentials: true,
   })
 );
@@ -23,13 +25,12 @@ app.use(express.json());
 app.use(cookieParser());
 
 // routes
-app.get("/", (req: Request, res: Response) => {
+app.get("/", (req, res) => {
   res.send("Blog Zone Server is Running");
 });
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cjx1mmk.mongodb.net/?retryWrites=true&w=majority`;
-console.log("connected to database");
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -74,16 +75,10 @@ async function run() {
     const wishlistCollection = client.db("MetaBlogDB").collection("wishlist");
 
     //  auth related api
-    app.post("/jwt", async (req: Request, res: Response) => {
+    app.post("/jwt", async (req, res) => {
       const user = req.body;
 
-      const accessToken = process.env.ACCESS_TOKEN;
-
-      if (!accessToken) {
-        throw new Error("Access token not found");
-      }
-
-      const token = jwt.sign(user, accessToken, {
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
         expiresIn: "1h",
       });
 
@@ -96,7 +91,7 @@ async function run() {
         .send({ success: true });
     });
 
-    app.post("/logout", async (req: Request, res: Response) => {
+    app.post("/logout", async (req, res) => {
       const user = req.body;
       console.log(user);
       res.clearCookie("token", { maxAge: 0 }).send({ message: true });
@@ -105,8 +100,8 @@ async function run() {
     //*  blogs related api
 
     // ? search api
-    app.get("/blogs/search", async (req: Request, res: Response) => {
-      const regexPattern = new RegExp(req.query.include as string, "i");
+    app.get("/blogs/search", async (req, res) => {
+      const regexPattern = new RegExp(req.query.include, "i");
       try {
         const blogs = await blogsCollection
           .find({ title: regexPattern })
@@ -115,30 +110,30 @@ async function run() {
         return res
           .status(200)
           .send({ success: true, total: blogs.length, data: blogs });
-      } catch (error: string | any) {
+      } catch (error) {
         return res.status(500).send({ message: error.message });
       }
     });
 
     // ?recent blog api
 
-    app.get("/blogs/recent", async (req: Request, res: Response) => {
+    app.get("/blogs/recent", async (req, res) => {
       try {
         const blogs = await blogsCollection
           .find()
           .project({ authorEmail: 0 })
-          .sort({ published: -1 })
+          .sort({ published : -1 })
           .limit(9)
           .toArray();
         return res.status(200).send({ total: blogs.length, data: blogs });
-      } catch (error: string | any) {
+      } catch (error) {
         return res.status(500).send({ message: error.message });
       }
     });
 
     // ? all blogs api
 
-    app.get("/blogs", async (req: Request, res: Response) => {
+    app.get("/blogs", async (req, res) => {
       try {
         let query = {};
 
@@ -158,38 +153,38 @@ async function run() {
         return res
           .status(200)
           .json({ success: true, total: blogs.length, data: blogs });
-      } catch (error: string | any) {
+      } catch (error) {
         return res.status(500).send({ message: error.message });
       }
     });
 
     // ? create blog api
 
-    app.post("/blogs", async (req: Request, res: Response) => {
+    app.post("/blogs", async (req, res) => {
       try {
         const blog = req.body;
         const result = await blogsCollection.insertOne(blog);
         return res.status(201).send(result);
-      } catch (error: string | any) {
+      } catch (error) {
         return res.status(500).send({ message: error.message });
       }
     });
 
-    app.get("/blogs/:id", async (req: Request, res: Response) => {
+    app.get("/blogs/:id", async (req, res) => {
       try {
         const id = req.params.id;
         const query = { _id: new ObjectId(id) };
 
         const result = await blogsCollection.findOne(query);
         return res.status(200).send({ success: true, data: result });
-      } catch (error: string | any) {
+      } catch (error) {
         return res.status(500).send({ message: error.message });
       }
     });
 
     // ? update blog api
 
-    app.patch("/blogs/:id", async (req: Request, res: Response) => {
+    app.patch("/blogs/:id", async (req, res) => {
       try {
         const id = req.params.id;
         const query = { _id: new ObjectId(id) };
@@ -205,39 +200,39 @@ async function run() {
         };
         const result = await blogsCollection.updateOne(query, blog, options);
         return res.status(200).send({ success: true, data: result });
-      } catch (error: string | any) {
+      } catch (error) {
         return res.status(500).send({ message: error.message });
       }
     });
 
     // ? delete blog api
 
-    app.delete("/blogs/:id", async (req: Request, res: Response) => {
+    app.delete("/blogs/:id", async (req, res) => {
       try {
         const id = req.params.id;
         const query = { _id: new ObjectId(id) };
         const result = await blogsCollection.deleteOne(query);
         return res.status(200).send(result);
-      } catch (error: string | any) {
+      } catch (error) {
         return res.status(500).send({ message: error.message });
       }
     });
 
     //  comments related api
 
-    app.post("/comments", async (req: Request, res: Response) => {
+    app.post("/comments", async (req, res) => {
       const comment = req.body;
       const result = await CommentsCollection.insertOne(comment);
       res.send(result);
     });
 
-    app.get("/comments", async (req: Request, res: Response) => {
+    app.get("/comments", async (req, res) => {
       const cursor = CommentsCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
 
-    app.patch("/comments/:id", async (req: Request, res: Response) => {
+    app.patch("/comments/:id", async (req, res) => {
       const id = req.params.id;
 
       const query = { _id: new ObjectId(id) };
@@ -251,7 +246,7 @@ async function run() {
       res.send(result);
     });
 
-    app.delete("/comments/:id", async (req: Request, res: Response) => {
+    app.delete("/comments/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await CommentsCollection.deleteOne(query);
@@ -260,32 +255,32 @@ async function run() {
 
     //  wishlist related api
 
-    // app.post("/wishlist", async (req: Request, res: Response) => {
-    //   const wishlist = req.body;
-    //   const result = await wishlistCollection.insertOne(wishlist);
-    //   res.send(result);
-    // });
+    app.post("/wishlist", async (req, res) => {
+      const wishlist = req.body;
+      const result = await wishlistCollection.insertOne(wishlist);
+      res.send(result);
+    });
 
-    // app.get("/wishlist", async (req: Request, res: Response) => {
-    //   // if (req.query.email !== req.user.email) {
-    //   //   return res.status(403).send({ message: "forbidden" });
-    //   // }
+    app.get("/wishlist", async (req, res) => {
+      // if (req.query.email !== req.user.email) {
+      //   return res.status(403).send({ message: "forbidden" });
+      // }
 
-    //   let query = {};
-    //   if (req.query?.email) {
-    //     query = { email: req.query.email };
-    //   }
-    //   const cursor = wishlistCollection.find(query);
-    //   const result = await cursor.toArray();
-    //   res.send(result);
-    // });
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const cursor = wishlistCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-    // app.delete("/wishlist/:id", async (req: Request, res: Response) => {
-    //   const id = req.params.id;
-    //   const query = { _id: new ObjectId(id) };
-    //   const result = await wishlistCollection.deleteOne(query);
-    //   res.send(result);
-    // });
+    app.delete("/wishlist/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await wishlistCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
