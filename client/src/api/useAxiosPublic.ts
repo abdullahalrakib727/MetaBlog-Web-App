a903a9d2c07941b631b6e6a5e0c80c8183f1b7da
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-03-21 01:35:37 +0600

    fixed errors

--- client/src/api/useAxiosPublic.ts
+++ client/src/api/useAxiosPublic.ts
@@ -1,13 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const axiosPublic: AxiosInstance = axios.create({
-<<<<<<< HEAD
   baseURL: "https://blog-website-server-theta.vercel.app",
-=======
-
-  baseURL: "http;//localhost:5000",
-
->>>>>>> 5f5584f0d26d05f278ff37cf66a3a310fe135a73
 });
 
 const useAxiosPublic = () => {
