26f95359989c1651a42c5d12ac43fbba851033bd
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-03-28 00:06:39 +0600

    added logo on dashboard

--- client/src/components/SideNav/SideNav.tsx
+++ client/src/components/SideNav/SideNav.tsx
@@ -10,11 +10,23 @@ const SideNav = () => {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
-        className="mb-2 flex h-20 items-end justify-start rounded-md bg-[#E8E8EA] dark:bg-[#242535] p-4 md:h-40"
+        className="mb-2 flex h-20 items-center justify-start rounded-md bg-[#E8E8EA] dark:bg-[#242535] p-4 md:h-20"
         to="/"
       >
         <div className="w-32 dark:text-white md:w-40">
-          {/* <AcmeLogo /> */} Logo
+          <div>
+            <div className="hidden dark:block">
+              <img src="/images/dark-logo.png" alt="" />
+            </div>
+
+            <div>
+              <img
+                className="dark:hidden"
+                src="/images/light-logo.png"
+                alt=""
+              />
+            </div>
+          </div>
         </div>
       </Link>
       <button
