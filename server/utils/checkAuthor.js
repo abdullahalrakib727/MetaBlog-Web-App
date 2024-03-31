b549b8771b1d067d9ab6d591165ce50cf07e1c67
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-03-31 21:32:02 +0600

    added comments

--- server/utils/checkAuthor.js
+++ server/utils/checkAuthor.js
@@ -1,5 +1,7 @@
 const AllBlogs = require("../models/AllBlogs");
 
+//* Check if the user is an author
+
 const isAuthor = async (id) => {
   const query = { authorId: id };
   const user = await AllBlogs.findOne(query).select("authorId -_id");
--- server/utils/checkUser.js
+++ server/utils/checkUser.js
@@ -1,5 +1,7 @@
 const User = require("../models/User");
 
+//* Check if the user is an admin
+
 const isUser = async (id) => {
   const query = { uid: id };
   const user = await User.findOne(query).select("uid -_id");
