7312909bca156bc4a1814f802481edfd9a73fb70
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-06-08 11:12:29 +0600

    Update MongoDB connection URI

--- server/db/db.js
+++ server/db/db.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected");
   } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
+    console.log("error", error.message);
   }
 };
 
